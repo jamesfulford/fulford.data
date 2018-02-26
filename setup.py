c246de447a1929d5a8826b693334b6d6bf169ecb
James P. Fulford <james.patrick.fulford@gmail.com>
2018-02-26 09:36:00 -0500

    Initial Commit
    Start of fulford.data project as separate from Utility-Belt repository.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+from distutils.core import setup
+
+setup(
+    name="fulford-data",
+    description="Couple of data processing tools I find useful.",
+    author="James Patrick Fulford",
+    author_email="james.patrick.fulford@gmail.com",
+    url="https://github.com/jamesfulford/Utility-Belt",
+
+    version="0.1.0",
+
+    packages=["fulford.data"],
+)
