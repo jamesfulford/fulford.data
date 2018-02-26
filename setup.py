2d70846accd943da60d7d818111f528920ddbbeb
James P. Fulford <james.patrick.fulford@gmail.com>
2018-02-26 09:47:35 -0500

    Renamed directory to allow pip to work

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-
-
 from distutils.core import setup
 
+desc = "Declarative processing, transforming, and validating of data."
+
+kwargs = {
+    "name": "fulford.data",
+    "description": desc,
+    "author": "James Patrick Fulford",
+    "author_email": "james.patrick.fulford@gmail.com",
+    "url": "https://github.com/jamesfulford/fulford.data",
+
+    "version": "0.1.0",
+
+    "packages": ["fulforddata"],
+}
+
 setup(
-    name="fulford-data",
-    description="Couple of data processing tools I find useful.",
-    author="James Patrick Fulford",
-    author_email="james.patrick.fulford@gmail.com",
-    url="https://github.com/jamesfulford/Utility-Belt",
-
-    version="0.1.0",
-
-    packages=["fulford.data"],
+    **kwargs
 )
