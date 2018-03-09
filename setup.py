022e14c634b5e77841a45d859aa892bb4cc249d5
James P. Fulford <james.patrick.fulford@gmail.com>
2018-03-08 23:39:13 -0500

    Committing 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from distutils.core import setup
 from setuptools import find_packages
 
 
-desc = "Declarative processing, transforming, and validating of data."
+desc = """Declarative processing, transforming, and validating of data.
+
+New in 1.0.4:
+-   Fixed process.Trickler iteration to not be internally recursive, sometimes
+    throwing stack overflow errors on long waits.
+"""
 
 kwargs = {
     "name": "fulford.data",
@@ -12,7 +17,7 @@ kwargs = {
     "url": "https://github.com/jamesfulford/fulford.data",
     "license": "Apache-2.0",
 
-    "version": "1.0.3",
+    "version": "1.0.4",
 
     "packages": find_packages()
 }
